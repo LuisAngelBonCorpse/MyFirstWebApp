8ffa821cc7ee95a745d61360c39e8533d1803f3e
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-11-02 21:51:37 -0600

    Initial commit

---
+++ src/main/java/org/generation/MyFirstWebApp/controller/UserController.java
@@ -0,0 +1,42 @@
+package org.generation.MyFirstWebApp.controller;
+
+import org.generation.MyFirstWebApp.model.User;
+import org.generation.MyFirstWebApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController
+{
+    final UserService userService;
+
+    public UserController( @Autowired UserService userService )
+    {
+        this.userService = userService;
+        userService.add( new User( 1, "Santiago", "Carrillo" ) );
+        userService.add( new User( 2, "Santi", "Carrillo" ) );
+        userService.add( new User( 3, "Sago", "Carrillo" ) );
+    }
+
+    @GetMapping( "/user" )
+    public User getUser()
+    {
+        return userService.getUser( 0 );
+    }
+
+    @GetMapping( "/userNumberAll" )
+    public int totalUsers()
+    {
+        return userService.size();
+    }
+
+    @GetMapping( "/allUsers" )
+    public List<User> getUsers()
+    {
+            return userService.getAll();
+    }
+    
+}
